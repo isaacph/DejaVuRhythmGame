13f7cb12dd6a4b70812f9c6b9c48b22b06f3e347
isaacph <isaacphuffman@gmail.com>
2021-10-16 01:03:42 -0400

    Renaming...

--- src/main/java/game/Game.java
+++ src/main/java/game/Game.java
@@ -17,7 +17,7 @@ import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.system.MemoryUtil.*;
 
-public class Main {
+public class Game {
 
     // The window handle
     private long window;
@@ -205,7 +205,7 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        new Main().run();
+        new Game().run();
     }
 
     private void onWindowSize(int x, int y) {
