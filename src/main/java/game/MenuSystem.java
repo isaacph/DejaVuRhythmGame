b97d116c452f065d7771c85ad35ef52e6e9e9f27
Musa <musah@umich.edu>
2021-10-17 04:01:32 -0400

    Added logo

--- src/main/java/game/MenuSystem.java
+++ src/main/java/game/MenuSystem.java
@@ -9,6 +9,7 @@ public class MenuSystem implements GameSystem {
 
     private final Game game;
     private Consumer<State> finishFunc;
+    private Texture logoTexture;
 
     public MenuSystem(Game game) {
         this.game = game;
@@ -16,6 +17,7 @@ public class MenuSystem implements GameSystem {
 
     @Override
     public void init() {
+        logoTexture = Texture.makeTexture("sprites/logo.png");
         game.tutorialButton.show = true;
         game.playButton.show = true;
         game.musicPlayer.show = false;
@@ -39,8 +41,10 @@ public class MenuSystem implements GameSystem {
 
     @Override
     public void render() {
-        String txt = "Test Menu";
-        float width = game.bigFont.textWidth(txt);
-        game.bigFont.draw("Test Menu", game.screenSize.x / 2.0f - width / 2.0f, game.screenSize.y / 4.0f, new Matrix4f(game.ortho), new Vector4f(1, 1, 1, 1));
+        // String txt = "Test Menu";
+        // float width = game.bigFont.textWidth(txt);
+        // game.bigFont.draw("Test Menu", game.screenSize.x / 2.0f - width / 2.0f, game.screenSize.y / 4.0f, new Matrix4f(game.ortho), new Vector4f(1, 1, 1, 1));
+        logoTexture.bind();
+        game.drawTexture.draw(new Matrix4f(game.ortho).translate(game.screenSize.x / 2.0f - (170 / 2), game.screenSize.y / 4.0f, 0).scale(100), new Vector4f(1.0f, 1.0f, 1.0f, 1.0f));
     }
 }
